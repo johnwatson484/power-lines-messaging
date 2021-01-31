7b3d656f2b67f994ffae039230d953d1eea24de9
John Watson <johnwatson484@hotmail.com>
2021-01-31 16:52:40 +0000

    Update connection interface

--- PowerLinesMessaging/Connection/IConnection.cs
+++ PowerLinesMessaging/Connection/IConnection.cs
@@ -2,6 +2,9 @@ namespace PowerLinesMessaging
 {
     public interface IConnection
     {
+        Consumer CreateConsumerChannel(ConsumerOptions options);
+        Sender CreateSenderChannel(SenderOptions options);
+        void CloseChannel(string name);
         void CloseConnection();
     }
 }
